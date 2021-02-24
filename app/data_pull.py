a6372bdf4938e8c49014ea4e066f987623b65e32
nyawanga <francodhi@gmail.com>
2021-02-24 23:35:11 +0300

    working data pull no processor

--- app/data_pull.py
+++ app/data_pull.py
@@ -18,39 +18,51 @@ configs = {
     "app_path" : os.path.dirname(os.path.abspath(__file__)),
     "base_path" : os.path.join( os.path.dirname(os.path.abspath(__file__)), "../") ,
     "google_auth" : os.path.join( os.path.join( os.path.dirname(os.path.abspath(__file__)), "../"), "lib/google_auth.json" ) ,
+    "worksheet_key": "",                                      # get this from the sheet url
+    "tab_name": "Data",                                                                                   # the google sheet tab to post the data into
 }
 
 
 class DataPull():
 
-    def __init__(self, google_auth):
-        self.google_auth = google_auth
-        self.gcreds = pygsheets.authorize(service_file=self.google_auth)
+    def __init__(self, configs):
+        self.configs = configs
+        self.gcreds = pygsheets.authorize(service_file=configs.get( "google_auth" ))
         self.data = None
 
-    def pull_data(self, tab_name):
+    def pull_from_sheet(self, tab_name):
 
         sheet_access = self.gcreds.open_by_key( self.configs.get("worksheet_key") )
-        first_row = start_cell[0]
-        last_row = rows + 1
+        # first_row = start_cell[0]
+        # last_row = rows + 1
 #         end_cell = (rows, cols)
 
         try:
             tab = sheet_access.worksheet_by_title(tab_name)
-            data = tab.get_As_Df(has_header=True, include_tailing_empty=False)
+            data = tab.get_as_df(has_header=True, numerize=True, empty_value=None, include_tailing_empty=False)
+
+            # process the dataframe here
             self.data = data
             # return data
 
+        except pygsheets.exceptions.WorksheetNotFound:
+            print(f"{tab_name} sheet not found")
+            sys.exit(1)
         except Exception as err:
-            raise
+            print(f"{err} got when getting the sheet {tab_name}")
+        finally:
+            print("-" * 100)
 
 def main():
-    etl = DataPull( configs.get("google_auth") )
-    etl.pull_data( "Data" )
+    etl = DataPull( configs )
+    etl.pull_from_sheet( "Data" )
     data = etl.data
+    return data
 
 
 if __name__ == "__main__":
     data = main()
     print( data )
+    # print( os.listdir( configs.get("base_path")) )
+    # print( configs.get("google_auth") ) #, configs.get("google_auth")) )
 
